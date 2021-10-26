d058b5f10bad0543f69813e586b41a8e9ad10546
Daniela Montserrat Enriquez Ballesteros <82247636+HatshepsutS@users.noreply.github.com>
2021-10-25 22:15:13 -0500

    Update main.c
    Escribi un menu

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+int main()
+{	
+
+ printf("Bienvenido al simulador de memoria virtual :) \n");
+ printf("1. Llenar la memoria RAM \n");
+ printf("2. Insertar pagina a la memoria fisica\n"); 
+ printf("3. Ver estado de memoria\n"); 
+		
+
+}
