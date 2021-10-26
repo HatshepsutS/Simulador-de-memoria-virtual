cb9ea309526e95e4f1df3a60f78f72beacae7433
HaniHoney <ggshappy@naver.com>
2021-10-25 23:15:31 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lista.h"
+#define sizeMV 64 //tamaño de memoria virtual
+#define sizeMF 32 //tamaño de memoria física
+#define sizePG 4 //tamaño de la página
+
 int main()
 {	
 
@@ -9,5 +13,7 @@ int main()
  printf("2. Insertar pagina a la memoria fisica\n"); 
  printf("3. Ver estado de memoria\n"); 
 		
+Lista ListMV=cons();
+for(int i=0;i<(sizeMV/sizePG);i++){
 
 }
