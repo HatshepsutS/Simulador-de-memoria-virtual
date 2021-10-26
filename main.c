52f40239103166cd371369934c984e0c158d951d
HaniHoney <ggshappy@naver.com>
2021-10-26 15:00:03 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "lista.h"
 #define sizeMV 64 //tamaño de memoria virtual
 #define sizeMF 32 //tamaño de memoria física
@@ -16,66 +17,106 @@ int binary(int n){
     return (int)binaryint;
 }
 
-void asignarproceso(int numpagina){
+int main(){
+
+    //Creando la tabla de páginas de la memoria virtual
+    int pagina[5][sizeMV/sizePG];
+    print("ID\tMemFi\tPA");
+    for(int i=0; i<(sizeMV/sizePG); i++){
+        pagina[0][i]=i;
+        pagina[1][i]=0;
+        pagina[2][i]=0;
+        pagina[3][i]=0;
+        pagina[4][i]=0; // presente=1 , ausencia=0
+        printf("%d\t%d%d%d\t%d\n",pagina[0][i],pagina[1][i],pagina[2][i],pagina[3][i],pagina[4][i]);
+    }
+    //Creando la tabla de marcos de la memoria física
+    int marco[6][sizeMF/sizePG];
+    print("ID\tMemVi");    
+    for(int j=0; j<(sizeMV/sizePG)+1; j++){
+        marco[0][j]=j;
+        marco[1][j]=0;
+        marco[2][j]=0;
+        marco[3][j]=0;
+        marco[4][j]=0;
+        marco[5][j]=0; //ocupado / no ocupado //ocultado
+        printf("%d\t%d%d%d%d\n",marco[0][j],marco[1][j],marco[2][j],marco[3][j],marco[4][j]);
+    }
+
+    List l=nueva(); //Lista se guarda el orden de pagina insertada
+
+    printf("Bienvenido al simulador de memoria virtual :) \n");
+    printf("1. Ver estado de memoria \n");
+    printf("2. Insertar pagina a la memoria fisica\n");
+    int select=0;
+    scanf("%d",&select);
+
+    switch(select){
+        case '1':
+            print("===Tabla de Paginas===");
+            print("\nIDpg\tMemFi\tPA");
+            for(int i=0; i<(sizeMV/sizePG); i++){            
+                printf("%d\t%d%d%d\t%d\n",pagina[0][i],pagina[1][i],pagina[2][i],pagina[3][i],pagina[4][i]);
+            }
+            print("===Tabla de Marcos===");
+            print("\nIDmr\tMemVi");    
+            for(int j=0; j<(sizeMV/sizePG); j++){
+                printf("%d\t%d%d%d%d\n",marco[0][j],marco[1][j],marco[2][j],marco[3][j],marco[4][j]);
+            }
+            break;
+        case '2':
+            int paginadesea, i=0;
+            printf("Ingrese el numero de la pagina se desea desea insertar a la memoria física: \n"); 
+            scanf("%d",&paginadesea);
+            while(pagina[5][i]!=0){ //hasta se encuentra marco ausencia
+                i++;
+                if(i==sizeMF/sizePG)
+                    break;
+            }
+            if(i==sizeMF/sizePG){ //en caso no hay marco vacio > sacando primer de la lista
+                i=0;//voy a poner al 0 de marco
+                int pripag = primerPag(l);
+                pagina[4][pripag]=0; // apagando P/A
+                l=desformar(l);
+            }
+            //poner en la lista, id marco, id pagina
+            l=formar(l,i,paginadesea);
+            //asignando id pagina en la tabla marco
+            marco[4][i] = paginadesea%2;
+            paginadesea=paginadesea/2;
+            marco[3][i] = paginadesea%2;
+            paginadesea=paginadesea/2;
+            marco[2][i] = paginadesea%2;
+            paginadesea=paginadesea/2;
+            marco[1][i] = paginadesea%2;
+            break;
+        
+        default:
+            printf("\nPor favor, Elige 1 o 2\n");
+
+
+    }
+
     
 
-    //avanza hasta encontrar bitprau == 0 con for iteración
-    
-    //si todos itprau == 1    
-}
-
-int main()
-{	
-
- printf("Bienvenido al simulador de memoria virtual :) \n");
- printf("1. Llenar la memoria RAM \n");
- printf("2. Insertar pagina a la memoria fisica\n"); 
- printf("3. Ver estado de memoria\n"); 
-
-
-int pagina[5][sizeMV/sizePG];
-//Creando la tabla de páginas de la memoria virtual
-print("ID\tMemFi\tPA");
-for(int i=0; i<(sizeMV/sizePG); i++){
-    pagina[0][i]=i;
-    pagina[1][i]=0;
-    pagina[2][i]=0;
-    pagina[3][i]=0;
-    pagina[4][i]=0; // presente=1 , ausencia=0
-    printf("%d\t%d%d%d\t%d\n",pagina[0][i],pagina[1][i],pagina[2][i],pagina[3][i],pagina[4][i]);
-}
-int marco[5][sizeMF/sizePG];
-print("ID\tMemVi");
-//Creando la tabla de marcos de la memoria física
-for(int j=0; j<(sizeMV/sizePG); j++){
-    marco[0][j]=j;
-    marco[1][j]=0;
-    marco[2][j]=0;
-    marco[3][j]=0;
-    marco[4][j]=0;
-    printf("%d\t%d%d%d%d\n",marco[0][j],marco[1][j],marco[2][j],marco[3][j],marco[4][j]);
-}
 
 
 
+    Pag p = (Pag) malloc(sizeof(struct pnodo));
+    for(int i=0;i<(sizeMV/sizePG);i++){
+        p -> indice = i;
+        p -> numpag = "0000";
+        p -> bitprau = 0;
+        Lista ListMV=cons(p,vacia());
+    }
 
-
-
-Pag p = (Pag) malloc(sizeof(struct pnodo));
-for(int i=0;i<(sizeMV/sizePG);i++){
-	p -> indice = i;
-	p -> numpag = "0000";
-	p -> bitprau = 0;
-    Lista ListMV=cons(p,vacia());
-}
-
-Vir v = (Vir) malloc(sizeof(struct vnodo));
-for(int i=0;i<(sizeMF/sizePG);i++){
-	p -> indice = i;
-	p -> numpag = "000";
-	p -> bitprau = 0;
-    Lista ListMV=cons(p,vacia());
-}
+    Vir v = (Vir) malloc(sizeof(struct vnodo));
+    for(int i=0;i<(sizeMF/sizePG);i++){
+        p -> indice = i;
+        p -> numpag = "000";
+        p -> bitprau = 0;
+        Lista ListMV=cons(p,vacia());
+    }
 
 return 0;
 }
